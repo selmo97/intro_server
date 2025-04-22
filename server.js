db819b75d88fc726723bba3fe83bb63ed3114f29
selmo97 <selena.moss2@gmail.com>
2025-04-22 16:24:04 -0700

    json routes

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
-import express from 'express'
+const express = require("express");
+const path = require('path');
 
-const app = express()
+//initializing express app
+const app = express();
+const PORT = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+const og1DMembers = [
+    'Harry',
+    'Liam',
+    'Louis',
+    'Niall',
+    'Zayn'
+];
+
+// creating a route
+app.get('/api/drama', (req, res) => {
+  res.send("Have you heard the tea????☕️✨");
+});
+
+//route that recieves the letter from the url
+app.get('/api/words/:letter', (req, res) => {
+  const { letter } = req.params;
+  res.send(`The letter you entered in your url is: ${letter}`);
+});
+
+//route that sends info to browser
+app.get('/api/onedirection', (req,res) => {
+    res.status(200).json(og1DMembers)
+});
+
+app.get('/onedirection', (req,res) => {
+    console.log('📑 /onedirection route HIT!')
+    res.sendFile(path.join(__dirname, '/onedirection.html'));
 })
 
-app.listen(3000)
+//starting server at a specific port
+app.listen(PORT, () => {
+  console.log("Server slaying on http://localhost:3001 📟");
+});
